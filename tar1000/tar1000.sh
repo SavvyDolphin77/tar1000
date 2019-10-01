7cee366dd87b6796362d9d7caa104215a197a9fa
SavvyDolphin77 <46875742+SavvyDolphin77@users.noreply.github.com>
2019-10-01 17:39:01 -0400

    v1.0.0
    Made program that successfully untared a file 1000 times. Included file for users to test my program if they wanted to.

---
+++ tar1000/tar1000.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# A program for untaring a user inputed amount of files (only works if .tar files are named for numbers in descending order).
+
+c=1000
+
+#sets up for loop for tar operation
+while [ c!=1 ]; do
+
+	#untars each time while for loop is true
+	tar -xvf $c.tar
+	c=`expr $c - 1`
+done
