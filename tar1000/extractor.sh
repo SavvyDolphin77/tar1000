5a3f781e6b1a6da72e5a3bdbf55e08aa7f2bbbd1
SavvyDolphin77 <46875742+SavvyDolphin77@users.noreply.github.com>
2020-05-13 18:07:53 -0400

    v1.0.2 | Fixed Issue #2
    Fixed issue #2: Exits With Error, where the script exited with an error. Now runs fine without it.

--- tar1000/extractor.sh
+++ tar1000/extractor.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # A program for untaring a user inputed amount of files (only works if .tar files are named for numbers in descending order).
+# <-- Conner Harbaugh | Copyright 2020 -->
 
 c=1000
 
-#sets up for loop for tar operation
-while [ $c -ge  0 ]; do
+#Sets conditions for loop by checking if the variable c is greater then 0
+#Then, while c is greater then zero runs the code inside the loop
+while [ $c -gt  0 ]; do
 
 	#untars each time while for loop is true
 	tar -xvf $c.tar
 	c=`expr $c - 1`
+
+#Ends the loop
 done
